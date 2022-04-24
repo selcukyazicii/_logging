1c148cc7c1dba34c588e4e65a5b4a929e5d1780a
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2022-04-24 16:21:45 +0300

    Add project files.

---
+++ _logging/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Lütfen sayı giriniz");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi>=10 && sayi<=50)
+                {
+                    throw new Exception("Sayı 10 ile 50 arasında olmamalıdır");
+                }
+                else
+                {
+                    throw new Exception("Sayı şartlara uygundur,giriş yapılabilir.");
+                }
+            }
+            catch (Exception error)
+            {
+                File.AppendAllText("log.txt", Environment.MachineName);
+                File.AppendAllText("log.txt", Environment.NewLine);
+                File.AppendAllText("log.txt", Environment.UserName);
+                File.AppendAllText("log.txt", DateTime.Now.ToString("dd.MM.yyyy HH:mm"));
+                File.AppendAllText("log.txt", "\r\n");
+                File.AppendAllText("log.txt", error.Message);
+                File.AppendAllText("log.txt", error.StackTrace);
+                File.AppendAllText("log.txt", Environment.NewLine+"************"+Environment.NewLine);
+            }
+        }
+    }
+}
